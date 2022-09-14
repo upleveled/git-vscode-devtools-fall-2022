f143ad68a2e7517b5997059207ac6fa40026b5b2
Victor Ejike Nwosu <ejikello123@yahoo.com>
2022-09-14 12:27:07 +0200

    Replace console.log with animal objects

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-console.log('Hello world');
-console.log('Hello world');
-
-console.log('Hello world');
-console.log('Hello world');
+const animals = [
+  { id: 1, animalName: 'god', age: 3 },
+  { id: 2, animalName: 'goat', age: 5 },
+  { id: 3, animalName: 'pig', age: 9 },
+  { id: 4, animalName: 'chicken', age: 7 },
+  { id: 5, animalName: 'elephant', age: 1 },
+  { id: 6, animalName: 'lion', age: 7 },
+  { id: 7, animalName: 'tiger', age: 8 },
+  { id: 8, animalName: 'cat', age: 3 },
+];
